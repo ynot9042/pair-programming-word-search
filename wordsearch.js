9d19d2639ac149f773b376880dcdf3aecaf16736
Jeffrey Ho <jeff.ho@hotmail.com>
2022-08-03 19:34:25 +0000

    finished function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// Paired Programming With Bailee Doherty
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  if (word === "") {
+    return undefined;
+  }
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  let result = false;
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      result = true;
+    }
+  }
+
+  let length = letters[0].length;
+
+  for (i = 0; i < length; i++) {
+    let verticalArray = [];
+    for (j = 0; j < letters.length; j++) {
+      verticalArray.push(letters[j][i]);
+    }
+    let verticalJoin = verticalArray.join("");
+
+    if (verticalJoin.includes(word)) {
+      result = true;
+    }
+  }
+  return result;
+};
+
+module.exports = wordSearch;
